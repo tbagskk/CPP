f78c1edd403b7d7a10e25614703741898c05c384
Gabin Cherquitte <gcherqui@k1r4p12.42mulhouse.fr>
2023-09-14 09:56:52 +0200

    cpp4

--- cpp4/ex02/main.cpp
+++ cpp4/ex02/main.cpp
@@ -1,21 +1,12 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "AAnimal.hpp"
 
 int main()
 {
-	int	size = 2;
-	Animal *animals[size];
-
-	for(int i = 0; i < size; i++)
-	{
-		if (i < size / 2)	
-			animals[i] = new Dog();
-		else
-			animals[i] = new Cat();
-	}
-
-	for(int i = 0; i < size; i++)
-		delete animals[i];
+	//Animal tunsinge;
+	
+	Cat Chien;
 
 	return (0);
 
