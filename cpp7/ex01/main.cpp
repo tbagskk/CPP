9be8e619bc3dd0f4af3d44808acd23bcb04e3d8f
tbagskk <tbag.sk@gmail.com>
2024-01-12 14:48:28 +0100

    cpp7

--- cpp7/ex01/main.cpp
+++ cpp7/ex01/main.cpp
@@ -3,13 +3,17 @@
 template <typename T>
 void	func(T &value)
 {
-	value *= 2;
+	value *= 10;
 }
 
 int main( void )
 {
 	int	tab[5] = { 1, 2, 3, 4, 5};
+
 	iter(tab, 5, &func);
-	for(int i = 0; i < 5; i++)
+
+	for(int i = 0; i < 5; i++){
 		std::cout << tab[i] << std::endl;
+	}
+	
 }
